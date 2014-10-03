c75394f3418abf6cb43b4c06cafa2981b3efc827
Tom Boutell <tom@punkave.com>
2014-10-03 18:20:40 -0400

    Tell the asset manager that ckeditor is preshrunk

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function Construct(options, callback) {
 
   // Tells ckeditor where to find the rest of itself despite minification
   self.pushAsset('script', 'beforeCkeditor', { when: 'user' });
-  self.pushAsset('script', 'vendor/ckeditor/ckeditor', { when: 'user' });
+  self.pushAsset('script', 'vendor/ckeditor/ckeditor', { when: 'user', preshrunk: true });
   self.pushAsset('script', 'editor', { when: 'user' });
   self.pushAsset('stylesheet', 'editor', { when: 'user' });
   self.pushAsset('stylesheet', '../js/vendor/ckeditor/skins/apostrophe/editor', { when: 'user' });
